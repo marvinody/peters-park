8ff868c5d3cf7a45b0ef28fbeaae09090a4d91af
Manuel Bugallo <marvinody@gmail.com>
2019-10-09 09:21:30 -0400

    chore: adds comments to server index

--- src/server/index.js
+++ src/server/index.js
@@ -5,18 +5,30 @@ const app = express();
 const chalk = require('chalk')
 const { db } = require('./db')
 
+// logging middleware (different kind of morgan)
 app.use(volleyball)
+// parse incoming request bodies and convert them to some kinda JSONy object
 app.use(express.json())
 
+// where is all our generic stuff located? our images, stylestuff...
 const staticFolder = path.join(__dirname, '..', '..', 'static')
+// and let's tell express to serve STATIC assets from our STATIC folder
+// the folder could be named mango. doesn't matter. just makes sense to call it static
 app.use(express.static(staticFolder))
 
+// add our custom api router
 app.use('/api', require('./api'))
 
+// notice, this line is AFTER our other stuff
+// this says, if anyone requests ANYTHING ELSE, send them the index.html page
+// which will load React and then you can present a route if nothing matches
 app.use('*', (req, res) => {
   res.sendFile(path.join(staticFolder, 'index.html'))
 })
 
+// if a handler function has 4 parameters, it's an error handler
+// this is just a lot of code because it simplifies the error code you get by ALOT
+// love this code. it's amazing
 // custom error handling from https://github.com/FullstackAcademy/codys-cafe
 app.use((err, req, res, next) => {
   // just in case
@@ -38,6 +50,7 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).end()
 })
 
+// Finally, let's actually listen to requests
 app.listen(3000, () => {
   console.log("Hello Seattle. I'm listening")
   db.sync({ force: true })
