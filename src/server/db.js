e1ddff9aace7c8bb83772a2b4706f42360ff27ed
Manuel Bugallo <marvinody@gmail.com>
2019-10-09 12:35:07 -0400

    feat: updates models to include more variety

--- src/server/db.js
+++ src/server/db.js
@@ -18,6 +18,24 @@ const Cat = db.define('cat', {
   }
 })
 
+const Toy = db.define('toy', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+})
+
+const ToyLikeness = db.define('toyLikeness', {
+  rating: {
+    // 1 - 5
+    type: Sequelize.INTEGER,
+    defaultValue: 3
+  }
+})
+
+// many to many friends
+// many to many toy thing
+
 module.exports = {
   db, Cat
 }
