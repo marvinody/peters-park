5df273c3f4e9994dadd6436de603f2c105816ad8
Manuel Bugallo <marvinody@gmail.com>
2019-10-10 11:08:55 -0400

    feat: updates starting point for single cat

---
+++ src/client/components/SingleCat.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+// why are we exporting so much stuff you might ask?
+// and I would say good question
+// long story short, I need access to this stuff to test it
+// so we export it
+// by calling it disconnected (from the redux store), it's clear that
+// this is not the correct import you want generally
+// you want the default export
+// don't worry about it too much
+export const DisconnectedSingleCat = props => {
+  return (
+    <div class='single-cat'>
+
+    </div >
+  )
+}
+
+// the mapBlankToProps don't have to be filled out with anything
+// but use them to your advantage
+export const mapStateToProps = (state) => {
+  return {
+
+  }
+}
+
+export const mapDispatchToProps = (dispatch) => {
+  return {
+
+  }
+}
+
+// don't touch this line
+// but this is the component you probably want in most cases
+// so if you're using DisconnectedSingleCat somewhere else in your program
+// you're gonna have a bad time
+export default connect(mapStateToProps, mapDispatchToProps)(DisconnectedSingleCat)
+
+
+
